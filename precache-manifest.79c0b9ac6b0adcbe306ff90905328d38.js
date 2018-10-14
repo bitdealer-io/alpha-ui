297ba6bacfe1fd0136a8df6f1242e282055004c6
Marco Argentieri <marco@vulpem.com>
2018-10-15 00:16:11 +0200

    bb5f568

---
+++ precache-manifest.79c0b9ac6b0adcbe306ff90905328d38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f183517476dd94eeed04",
+    "url": "/alpha-ui/static/js/runtime~main.f1835174.js"
+  },
+  {
+    "revision": "b8b49bf35d2ccfcb8b99",
+    "url": "/alpha-ui/static/js/main.b8b49bf3.chunk.js"
+  },
+  {
+    "revision": "bffc7e95c191e4bbfe96",
+    "url": "/alpha-ui/static/js/1.bffc7e95.chunk.js"
+  },
+  {
+    "revision": "b8b49bf35d2ccfcb8b99",
+    "url": "/alpha-ui/static/css/main.277ba563.chunk.css"
+  },
+  {
+    "revision": "4f8bce15ef6016597327fca3f8e56b86",
+    "url": "/alpha-ui/index.html"
+  }
+];
